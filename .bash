28a289baedeefb96a8b287537cf027d6399d6fbc
bethw3l <kipkoechbethwel721@gmail.com>
2026-02-16 20:02:46 -0800

    Create .bash

---
+++ .bash
@@ -0,0 +1,5 @@
+npm create vite@latest stats-platform -- --template react-ts
+cd stats-platform
+npm install
+npm install plotly.js-dist-min react-plotly.js mathjs jstat
+npm install --save-dev gh-pages
